12e01e76fd367f79b999203bfd05499c11a8bdb1
Aviral Shastri <noobgamer9827539869@gmail.com>
2024-02-10 23:43:44 +0530

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-export default function Header() {
+import { useRouter } from 'next/router';
+
+const Header = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const router = useRouter();
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
   };
 
   return (
@@ -22,12 +29,7 @@ export default function Header() {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
           </svg>
           <svg
             className={`w-6 h-6 ${isMenuOpen ? 'block' : 'hidden'}`}
@@ -36,70 +38,87 @@ export default function Header() {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
 
         <nav
           className={`lg:flex lg:items-center ${
-            isMenuOpen ? 'fixed top-0 right-0 h-screen w-64 bg-white z-50 overflow-y-auto transition-transform duration-300 ease-in-out transform' : 'hidden'
+            isMenuOpen
+              ? 'fixed top-0 right-0 h-screen w-64 bg-white z-50 overflow-y-auto transition-transform duration-300 ease-in-out transform'
+              : 'hidden'
           }`}
         >
           <div className="lg:flex items-center">
-            <div className="lg:flex-shrink-0 lg:mr-6">
-              <Link href={'/'} passHref>
-                <div
-                  className="font-bold nav-item text-gray-800 cursor-pointer py-2 px-4 hover:bg-gray-200"
-                  onClick={toggleMenu}
-                >
-                  HOME
-                </div>
-              </Link>
-            </div>
-            <div className="lg:flex-shrink-0 lg:mr-6">
-              <Link href={'/about'} passHref>
-                <div
-                  className="font-bold nav-item text-gray-800 cursor-pointer py-2 px-4 hover:bg-gray-200"
-                  onClick={toggleMenu}
-                >
-                  ABOUT
-                </div>
-              </Link>
-            </div>
-            <div className="lg:flex-shrink-0 lg:mr-6">
-              <Link href={'/work'} passHref>
-                <div
-                  className="font-bold nav-item text-gray-800 cursor-pointer py-2 px-4 hover:bg-gray-200"
-                  onClick={toggleMenu}
-                >
-                  WORK
-                </div>
-              </Link>
-            </div>
-            <div className="lg:flex-shrink-0">
-              <Link href={'/contact'} passHref>
-                <div
-                  className="font-bold nav-item text-gray-800 cursor-pointer py-2 px-4 hover:bg-gray-200"
-                  onClick={toggleMenu}
-                >
-                  CONTACT
-                </div>
-              </Link>
-            </div>
+            <Link href={'/'} passHref>
+              <div
+                className={`font-bold nav-item text-gray-800 cursor-pointer py-2 px-4 hover:bg-gray-200 ${
+                  router.pathname === '/' ? 'active' : ''
+                }`}
+                onClick={() => {
+                  toggleMenu();
+                  if (router.pathname === '/') {
+                    closeMenu();
+                  }
+                }}
+              >
+                HOME
+              </div>
+            </Link>
+            <Link href={'/about'} passHref>
+              <div
+                className={`font-bold nav-item text-gray-800 cursor-pointer py-2 px-4 hover:bg-gray-200 ${
+                  router.pathname === '/about' ? 'active' : ''
+                }`}
+                onClick={() => {
+                  toggleMenu();
+                  if (router.pathname === '/about') {
+                    closeMenu();
+                  }
+                }}
+              >
+                ABOUT
+              </div>
+            </Link>
+            <Link href={'/work'} passHref>
+              <div
+                className={`font-bold nav-item text-gray-800 cursor-pointer py-2 px-4 hover:bg-gray-200 ${
+                  router.pathname === '/work' ? 'active' : ''
+                }`}
+                onClick={() => {
+                  toggleMenu();
+                  if (router.pathname === '/work') {
+                    closeMenu();
+                  }
+                }}
+              >
+                WORK
+              </div>
+            </Link>
+            <Link href={'/contact'} passHref>
+              <div
+                className={`font-bold nav-item text-gray-800 cursor-pointer py-2 px-4 hover:bg-gray-200 ${
+                  router.pathname === '/contact' ? 'active' : ''
+                }`}
+                onClick={() => {
+                  toggleMenu();
+                  if (router.pathname === '/contact') {
+                    closeMenu();
+                  }
+                }}
+              >
+                CONTACT
+              </div>
+            </Link>
           </div>
         </nav>
-        <div className="lg:flex-shrink-0 hidden lg:block">
+
+        <div className="lg:flex-shrink-0">
           <Image src="/logo.png" alt="Logo" width={50} height={50} />
-        </div>
-        <div className="lg:hidden block">
-          <Image src="/logo.png" alt="Logo" width={40} height={40} />
         </div>
       </div>
     </header>
   );
-}
+};
+
+export default Header;
