2e8913163fd92a1381cc8e06fc11b217b757a57b
Aviral Shastri <104085688+aviralshastri@users.noreply.github.com>
2024-06-08 15:01:45 +0530

    Update page.js

--- components/page.js
+++ components/page.js
@@ -30,7 +30,6 @@ export default function Page({ children }) {
             >
                 {children}
             </motion.main>
-            <Footer />
             <ScrollToTopButton />
         </>
     );
