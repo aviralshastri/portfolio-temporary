5e8fbaa7772efb056eb551e1b7d9a0ceb1bd5185
Aviral Shastri <104085688+aviralshastri@users.noreply.github.com>
2024-06-08 14:51:42 +0530

    SEO improvement

--- components/page.js
+++ components/page.js
@@ -1,19 +1,38 @@
 import Footer from "./Footer";
-import Header from "./header";
+import Header from "./Header";
 import { easeInOut, motion } from 'framer-motion';
-import ProgressBar from "./progressBar";
+import ProgressBar from "./ProgressBar";
 import ScrollToTopButton from "./ScrollToTopButton";
+import Head from 'next/head';
 
 export default function Page({ children }) {
-    
     return (
         <>
-            <motion.main initial={{ opacity: 0, y: 20 }}
+            <Head>
+                <title>Portfolio Aviral Shastri</title>
+                <meta name="description" content="Welcome to the portfolio of Aviral Shastri. Discover projects, skills, and contact information." />
+                <meta name="keywords" content="Aviral Shastri, Portfolio, Web Developer, Projects, Skills" />
+                <meta name="author" content="Aviral Shastri" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta property="og:title" content="Portfolio Aviral Shastri" />
+                <meta property="og:description" content="Welcome to the portfolio of Aviral Shastri. Discover projects, skills, and contact information." />
+                <meta property="og:url" content="https://aviral-shastri-portfolio.vercel.app/" />
+                <meta property="og:image" content="https://aviral-shastri-portfolio.vercel.app/me.jpg" />
+                <meta name="twitter:title" content="Portfolio Aviral Shastri" />
+                <meta name="twitter:description" content="Welcome to the portfolio of Aviral Shastri. Discover projects, skills, and contact information." />
+                <meta name="twitter:image" content="https://aviral-shastri-portfolio.vercel.app/me.jpg" />
+            </Head>
+            <Header />
+            <motion.main 
+                initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: 20 }}
-                transition={{ duration: 0.6, ease: easeInOut, delay: 0.2 }}>
+                transition={{ duration: 0.6, ease: easeInOut, delay: 0.2 }}
+            >
                 {children}
             </motion.main>
+            <Footer />
+            <ScrollToTopButton />
         </>
-    )
+    );
 }
